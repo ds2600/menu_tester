5b5a46615d2373e7196eb59a1404994da68923f6
ds2600 <bill@ishiganto.com>
2025-09-22 10:10:26 -0400

    moved everything to __init__

--- src/menu_tester/__init__.py
+++ src/menu_tester/__init__.py
@@ -1,1 +1,17 @@
-from .plugin import config
+from netbox.plugins import PluginConfig
+
+class MenuTesterConfig(PluginConfig):
+    name = "menu_tester"
+    verbose_name = "Menu Tester"
+    description = "Simplest test plugin to show a menu item."
+    version = "0.1.0"
+    base_url = "menu-tester"
+    default_settings = {
+        'top_level_menu': True,
+    }
+
+    def ready(self):
+        super().ready()
+
+
+config = MenuTesterConfig
